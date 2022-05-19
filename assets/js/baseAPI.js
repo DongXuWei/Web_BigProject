3483828530ae899fa75595331abdac6ba6ac9672
xuweidong <13016742879@163.com>
2022-05-19 17:16:53 +0800

    123

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -10,21 +10,24 @@ $.ajaxPrefilter(function(options) {
         }
     }
 
-    options.complete = function(res) {
-        // 解构对象的方式判断
-        const { code, message } = res.responseJSON
-        if (code !== 0) {
-            localStorage.removeItem('token')
-            location.href = './login.html'
-        }
-    }
-
-    //挂载权限回调
     // options.complete = function(res) {
-    //     if (res.responseJSON.code == 1 && res.responseJSON.message == '身份认证失败！') {
-    //         //console.log(res.responseJSON)
-    //         location.href = './login.html';
+    //     //     // 解构对象的方式判断
+    //     const { code, message } = res.responseJSON;
+    //     // && message != "身份认证失败！"   '更新密码成功！'
+    //     console.log(code)
+    //     console.log(message)
+    //     if (code !== 0) {
     //         localStorage.removeItem('token');
+    //         // location.href = './login.html'
     //     }
     // }
+
+    // 挂载权限回调
+    options.complete = function(res) {
+        if (res.responseJSON.code !== 0 && res.responseJSON.message == '身份认证失败！') {
+            //console.log(res.responseJSON)
+            location.href = './login.html';
+            localStorage.removeItem('token');
+        }
+    }
 })
